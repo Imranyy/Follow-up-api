6769de5225973527bad9846eaed75f64a45c5fdc
imranyy <imranmatano6@gmail.com>
2023-01-04 16:04:15 +0300

    modified documentation

--- views/js/app.js
+++ views/js/app.js
@@ -1,2 +1,7 @@
-
+const info=`You are a loser🤣🤣, thinking you can crack this app!!... I'm watching you☠ `
+const link='https://imran-matano.web.app/blogs'
+console.log(`
+    ${info}
+    Visit ${link}
+    `)
 
